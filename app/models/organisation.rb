237e150dc99c8912d3a47d0f146259668a147a89
Richard Schaubeck <richard.schaubeck@gmail.com>
2021-08-03 18:35:48 -0400

    annotated models with schema info

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: organisations
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  hourly_rate :decimal(12, 2)   not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Organisation < ApplicationRecord
 
 end
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: shifts
+#
+#  id           :bigint           not null, primary key
+#  user_id      :integer          not null
+#  start        :datetime         not null
+#  finish       :datetime         not null
+#  break_length :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class Shift < ApplicationRecord
 
 end
