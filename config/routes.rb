152b8df1e8e2e8874ec28c0d739121000acde700
Richard Schaubeck <richard.schaubeck@gmail.com>
2021-08-04 15:17:57 -0400

    changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'sessions#new'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
   get '/login' => 'sessions#new'
