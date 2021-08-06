7efa0925bc002890a5bf39109f3aee0aa68ff813
Richard Schaubeck <richard.schaubeck@gmail.com>
2021-08-06 18:41:28 -0400

    users can now edit and update an organisation

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,3 +1,23 @@
 class OrganisationsController < ApplicationController
+        
+    def edit
+        @org = Organisation.find(params[:id])
+    end
 
+    def update
+        @org = Organisation.find(params[:id])
+        if @org.update(org_params)
+            redirect_to user_url(current_user)
+        else
+            flash.now[:errors] = @org.errors.full_messages
+            render :edit
+        end
+    end
+
+    private
+
+    def org_params
+        params.require(:organisation).permit(:name, :hourly_rate)
+    end
+    
 end
