d14bda0a72493154470cf81f7a52f556810581ac
Richard Schaubeck <richard.schaubeck@gmail.com>
2021-08-10 17:56:47 -0400

    user can now create shifts

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 class ShiftsController < ApplicationController
     def index
         @org = Organisation.find(params[:organisation_id])
@@ -6,10 +8,27 @@ class ShiftsController < ApplicationController
     end
 
     def create
-
+        d = params[:shift][:day]
+        s = params[:shift][:start_time]
+        f = params[:shift][:finish_time]
+        params[:shift][:start] = "#{d} #{s}"
+        params[:shift][:finish] = "#{d} #{f}"
+        @shift = Shift.new(shift_params)
+        @shift.user_id = current_user.id
+        if @shift.save
+            redirect_to 
+        else
+            @org = Organisation.find(params[:organisation_id])
+            users = @org.users
+            get_shifts(users)
+            flash.now[:errors] = @shift.errors.full_messages
+            render :index
+        end
     end
 
-    def new
-    
+    private
+
+    def shift_params
+        params.require(:shift).permit(:start, :finish, :break_length)
     end
 end
